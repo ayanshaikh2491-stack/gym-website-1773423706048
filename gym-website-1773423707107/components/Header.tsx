adde6f1b4726bd7212dc41caa1ce9dd8af7c55ed
ayanshaikh2491-stack <ayanshaikh2491@gmail.com>
2026-03-13 23:11:48 +0530

    Initial commit - NEXUS AI Generated

---
+++ gym-website-1773423707107/components/Header.tsx
@@ -0,0 +1,37 @@
+import Link from 'next/link';
+import styles from '../styles/Header.module.css';
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <div className={styles.logo}>
+        <Link href="/">
+          <a>
+            <Image src="/logo.png" alt="Logo" width={120} height={60} />
+          </a>
+        </Link>
+      </div>
+      <nav className={styles.nav}>
+        <ul>
+          <li>
+            <Link href="/">
+              <a>Home</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/about">
+              <a>About</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/contact">
+              <a>Contact</a>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
